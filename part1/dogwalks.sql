7fffb9d83ac64e4e978e4502324fef7e494d8e54
TJgreentea <2282495604@qq.com>
2025-06-20 08:38:29 +0000

    [automatic commit] 2025-06-20 08:38:29 UTC 129.127.202.119

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -54,7 +54,7 @@ CREATE TABLE WalkRatings (
     CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
 );
 
--- Insert Users
+
 INSERT INTO Users (username, email, password_hash, role)
 VALUES
   ('alice123',  'alice@example.com', 'hashed123', 'owner'),
