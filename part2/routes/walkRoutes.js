0b472243cdd1634532bc263388e92d838d09d730
TJgreentea <2282495604@qq.com>
2025-06-20 19:49:10 +0000

    [automatic commit] 2025-06-20 19:49:10 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -14,9 +14,9 @@ router.get('/', async (req, res) => {
          wr.location,
          u.username       AS owner_username
          FROM WalkRequests wr
-        JOIN Dogs d  ON wr.dog_id  = d.dog_id
+         JOIN Dogs d  ON wr.dog_id  = d.dog_id
          JOIN Users u ON d.owner_id = u.user_id
-          WHERE wr.status = 'open'
+        WHERE wr.status = 'open'
    );
  // CHANGED: slimmed down SELECT to match /api/walkrequests/open response
     res.json(rows);
