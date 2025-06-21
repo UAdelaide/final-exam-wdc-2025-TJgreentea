183802bdc8c2d27f3dbacf0774988711bc0f0613
TJgreentea <2282495604@qq.com>
2025-06-21 06:38:14 +0000

    [automatic commit] 2025-06-21 06:38:14 UTC

---
+++ part2/routes/dogs.js
@@ -0,0 +1,21 @@
+app.get('/api/dogs', async (req, res) => {
+    try {
+      const pool = req.app.locals.pool;
+
+      const [rows] = await pool.query(`
+        SELECT
+          d.name         AS dog_name,
+          d.size,
+          u.username     AS owner_username
+        FROM Dogs d
+        JOIN Users u
+          ON d.owner_id = u.user_id
+      `);
+
+
+      res.json(rows);
+    } catch (err) {
+      console.error('Error in /api/dogs:', err);
+      res.status(500).json({ error: err.message });
+    }
+  });
