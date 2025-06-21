8188b61ed60d1707db3df77ed198920cf864a488
TJgreentea <2282495604@qq.com>
2025-06-21 07:13:41 +0000

    [automatic commit] 2025-06-21 07:13:40 UTC

--- part2/routes/dogs.js
+++ part2/routes/dogs.js
@@ -1,3 +1,6 @@
+const express=require('express');
+const app = express();
+const path = require('path');   
 app.get('/api/dogs', async (req, res) => {
     try {
       const pool = req.app.locals.pool;
